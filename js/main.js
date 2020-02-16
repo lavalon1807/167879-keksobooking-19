e7e2103cb35a90254f45bbc1b27550f41975a46d
Egor <Cost_benefit@mail.ru>
2020-02-16 19:06:29 +0300

    выставляем марки в свободном порядке

---
+++ js/main.js
@@ -0,0 +1,35 @@
+'use strict'
+
+var active = document.querySelector('.map');
+active.classList.remove('map--faded');
+
+var mapPins = document.querySelector('.map__pins');
+var templePin = document.querySelector('#pin')
+  .content
+  .querySelector('.map__pin');
+
+var PIN_MIN_X = 10;
+var PIN_MAX_X = 1200;
+var PIN_MIN_Y = 130;
+var PIN_MAX_Y = 630;
+
+/* Генерируем случайное число */
+var genNumber = function(min, max) {
+  var rend =  Math.floor(Math.random() * (max - min) + min);
+  return rend;
+};
+
+genNumber(PIN_MIN_X, PIN_MAX_X);
+console.log(genNumber(PIN_MIN_X, PIN_MAX_X));
+
+/* Задать координаты метки по X и Y */
+ for (var i = 0; i < 8; i++) {
+  var copyPin = templePin.cloneNode(true);
+  mapPins.appendChild(copyPin).style = 'left: '+ genNumber(PIN_MIN_X, PIN_MAX_X) + 'px; top: ' + genNumber(PIN_MIN_Y, PIN_MAX_Y) + 'px';
+ }
+
+
+
+
+
+

a0cf6ddb6f46e93870e334ab072ab9e5a6539094
Egor <Cost_benefit@mail.ru>
2020-02-16 19:13:42 +0300

    выставляем марки в свободном порядке

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 var active = document.querySelector('.map');
 active.classList.remove('map--faded');
@@ -14,22 +14,13 @@ var PIN_MIN_Y = 130;
 var PIN_MAX_Y = 630;
 
 /* Генерируем случайное число */
-var genNumber = function(min, max) {
-  var rend =  Math.floor(Math.random() * (max - min) + min);
+var genNumber = function (min, max) {
+  var rend = Math.floor(Math.random() * (max - min) + min);
   return rend;
 };
 
-genNumber(PIN_MIN_X, PIN_MAX_X);
-console.log(genNumber(PIN_MIN_X, PIN_MAX_X));
-
 /* Задать координаты метки по X и Y */
- for (var i = 0; i < 8; i++) {
+for (var i = 0; i < 8; i++) {
   var copyPin = templePin.cloneNode(true);
-  mapPins.appendChild(copyPin).style = 'left: '+ genNumber(PIN_MIN_X, PIN_MAX_X) + 'px; top: ' + genNumber(PIN_MIN_Y, PIN_MAX_Y) + 'px';
- }
-
-
-
-
-
-
+  mapPins.appendChild(copyPin).style = 'left: ' + genNumber(PIN_MIN_X, PIN_MAX_X) + 'px; top: ' + genNumber(PIN_MIN_Y, PIN_MAX_Y) + 'px';
+}

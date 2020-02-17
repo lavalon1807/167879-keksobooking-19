69946616d2daa6a2010f7ac9a69a373ffc666eb7
Egor <Cost_benefit@mail.ru>
2020-02-17 17:11:59 +0300

    Расположили аватарки в свободном, не повторяющемся порядке

--- js/main.js
+++ js/main.js
@@ -12,15 +12,32 @@ var PIN_MIN_X = 10;
 var PIN_MAX_X = 1200;
 var PIN_MIN_Y = 130;
 var PIN_MAX_Y = 630;
+/* Задаем множество разных аватарок */
+var patronPin = [];
+
+for (var i = 1; i <= 8; i++) {
+  patronPin[i] = {
+    author: {
+      avatar: 'img/avatars/user0' + (i + 1) + '.png'
+    }
+  };
+}
+
+var picture = templePin.querySelector('img');
 
 /* Генерируем случайное число */
+
 var genNumber = function (min, max) {
   var rend = Math.floor(Math.random() * (max - min) + min);
   return rend;
 };
 
-/* Задать координаты метки по X и Y */
-for (var i = 0; i < 8; i++) {
+
+/* Задать координаты метки по X и Y, аватарки */
+for (var j = 1; j <= 8; j++) {
   var copyPin = templePin.cloneNode(true);
   mapPins.appendChild(copyPin).style = 'left: ' + genNumber(PIN_MIN_X, PIN_MAX_X) + 'px; top: ' + genNumber(PIN_MIN_Y, PIN_MAX_Y) + 'px';
+  picture.setAttribute('src', patronPin[j].author.avatar);
 }
+
+

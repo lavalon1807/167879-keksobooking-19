e75197e0d0fac5727097df01a690c2094f53b965
Egor <Cost_benefit@mail.ru>
2020-02-17 20:57:35 +0300

    полное рандомное размещение меток

--- js/main.js
+++ js/main.js
@@ -4,40 +4,71 @@ var active = document.querySelector('.map');
 active.classList.remove('map--faded');
 
 var mapPins = document.querySelector('.map__pins');
+
 var templePin = document.querySelector('#pin')
   .content
   .querySelector('.map__pin');
 
-var PIN_MIN_X = 10;
-var PIN_MAX_X = 1200;
-var PIN_MIN_Y = 130;
-var PIN_MAX_Y = 630;
-/* Задаем множество разных аватарок */
-var patronPin = [];
-
-for (var i = 1; i <= 8; i++) {
-  patronPin[i] = {
-    author: {
-      avatar: 'img/avatars/user0' + (i + 1) + '.png'
-    }
-  };
-}
-
 var picture = templePin.querySelector('img');
 
-/* Генерируем случайное число */
+var PIN_MIN_X = 30;
+var PIN_MAX_X = 1100;
+var PIN_MIN_Y = 130;
+var PIN_MAX_Y = 630;
 
+var TITLES = [
+  'Уютное гнездышко для молодоженов',
+  'Старая хибара',
+  'Пентхаус',
+  'Клетка для голубей',
+  'Жилье без регистрации и СМС',
+  'Сдаю балкон',
+  'Комната для интроверта',
+  'Юрта для эскимоса'
+];
+
+/* генерирует случайное число */
 var genNumber = function (min, max) {
   var rend = Math.floor(Math.random() * (max - min) + min);
   return rend;
 };
 
+/* создание массива объектов случайных данных */
+var genPin = function () {
+  var patronPin = [];
+  for (var i = 0; i < 8; i++) {
+    var objPin = {
+      author: {
+        avatar: 'img/avatars/user0' + (i + 1) + '.png'
+      },
+      offer: {
+        title: TITLES[i]
+      }
+    };
+    patronPin.push(objPin);
+  }
+  return patronPin;
+};
 
-/* Задать координаты метки по X и Y, аватарки */
-for (var j = 1; j <= 8; j++) {
+var pins = genPin();
+
+/* генерация меток */
+var renderPin = function (data) {
   var copyPin = templePin.cloneNode(true);
-  mapPins.appendChild(copyPin).style = 'left: ' + genNumber(PIN_MIN_X, PIN_MAX_X) + 'px; top: ' + genNumber(PIN_MIN_Y, PIN_MAX_Y) + 'px';
-  picture.setAttribute('src', patronPin[j].author.avatar);
-}
+  copyPin.style = 'left: ' + genNumber(PIN_MIN_X, PIN_MAX_X) + 'px; top: ' + genNumber(PIN_MIN_Y, PIN_MAX_Y) + 'px';
+  picture.src = data.author.avatar;
+  picture.alt = data.offer.title;
 
+  return copyPin;
+};
 
+/* показывает метки на карте из фрагментов*/
+var addPins = function () {
+  var fragment = document.createDocumentFragment();
+  for (var j = 0; j < pins.length; j++) {
+    fragment.appendChild(renderPin(pins[j]));
+  }
+  mapPins.appendChild(fragment);
+};
+
+addPins();
